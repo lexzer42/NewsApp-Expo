86977949dd1d42b1548489d36b78e18ea7ca81e3
lexzer42 <oscaremilio.roj@gmail.com>
2025-03-09 15:01:45 -0300

    fix: update mock API key comment in Jest setup for clarity

--- jest.setup.cjs
+++ jest.setup.cjs
@@ -21,7 +21,7 @@ jest.mock('react-native-gesture-handler', () => {
 // Mock environment variables
 jest.mock('@env', () => ({
   NEWS_API_BASE_URL: 'https://newsapi.org/v2',
-  NEWS_API_KEY: '9818971a47af477182cc098ac4bc9933',
+  NEWS_API_KEY: '9818971a47af477182cc098ac4bc9933', // random number for mockup api key
   APP_ENV: 'test',
   EXPO_OS: 'android'
 }));
