91449a25e4c81702e73c8d342367318390c1072d
lexzer42 <oscaremilio.roj@gmail.com>
2025-03-09 15:07:36 -0300

    chore: format env.d.ts for consistency and readability

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -1,4 +1,4 @@
 declare module '@env' {
-    export const NEWS_API_BASE_URL: string;
-    export const NEWS_API_KEY: string;
-  }
+  export const NEWS_API_BASE_URL: string;
+  export const NEWS_API_KEY: string;
+}
